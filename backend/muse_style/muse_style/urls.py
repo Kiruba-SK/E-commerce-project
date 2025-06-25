aee0b4b66c3ae337253145e484631b3f4ec7330d
Kiruba-SK <kirubashangari13@gmail.com>
2025-06-25 22:31:48 +0530

    urls.py edited

--- backend/muse_style/muse_style/urls.py
+++ backend/muse_style/muse_style/urls.py
@@ -22,7 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apiApp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
