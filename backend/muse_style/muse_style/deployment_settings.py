a3be34b2035ed21cf5b6d7a6d93f95ebf45a01b5
Kiruba-SK <kirubashangari13@gmail.com>
2025-06-25 22:08:50 +0530

    deployed settings changed

--- backend/muse_style/muse_style/deployment_settings.py
+++ backend/muse_style/muse_style/deployment_settings.py
@@ -3,28 +3,34 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
+# Allowed hosts and CSRF settings
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
+# Production settings
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
-CORS_ORIGIN_WHITELIST = (
-                         https://e-commerce-frontend-dd1x.onrender.com,
-                        )
+# CORS configuration
+CORS_ALLOWED_ORIGINS = [
+    "https://e-commerce-frontend-dd1x.onrender.com",
+]
 
+# Static and media file storage
 STORAGES = {
     "default":{
         "BACKEND" : "django.core.files.storage.FileSystemStorage"
@@ -34,6 +40,7 @@ STORAGES = {
     }
 }
 
+# Database configuration
 DATABASES = {
     'default' : dj_database_url.config(
         default = os.environ['DATABASE_URL'],
--- backend/muse_style/muse_style/settings.py
+++ backend/muse_style/muse_style/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'Staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -132,8 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-                         'http://localhost:3000',
-                        )
-
-STATIC_ROOT = BASE_DIR/'Staticfiles'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', 
+]
