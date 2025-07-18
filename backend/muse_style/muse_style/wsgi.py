4c3413979a496aeabceec24269d70f940e9f8ba9
Kiruba-SK <kirubashangari13@gmail.com>
2025-07-18 20:33:52 +0530

    commit

--- backend/muse_style/muse_style/wsgi.py
+++ backend/muse_style/muse_style/wsgi.py
@@ -11,7 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'muse_style.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'muse_style.settings'
+# settings_module = 'muse_style.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'muse_style.settings'
+
+# If Railway sets an environment variable, detect it
+if 'RAILWAY_ENVIRONMENT' in os.environ or 'DATABASE_URL' in os.environ:
+    settings_module = 'muse_style.deployment_settings'
+else:
+    settings_module = 'muse_style.settings'
+    
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_wsgi_application()
