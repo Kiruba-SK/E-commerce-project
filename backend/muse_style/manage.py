52c676497b66d452482ccbe579545448795573ea
Kiruba-SK <kirubashangari13@gmail.com>
2025-07-18 21:15:01 +0530

    commit

--- backend/muse_style/manage.py
+++ backend/muse_style/manage.py
@@ -6,16 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # settings_module = 'muse_style.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'muse_style.settings'
 
-    # If Railway sets an environment variable, detect it
-    if 'RAILWAY_ENVIRONMENT' in os.environ or 'DATABASE_URL' in os.environ:
-        settings_module = 'muse_style.deployment_settings'
-    else:
-        settings_module = 'muse_style.settings'
+    settings_module = 'muse_style.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'muse_style.settings'
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )    
-    
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
